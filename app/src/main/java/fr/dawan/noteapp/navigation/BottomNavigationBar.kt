891f8b3fa339d6dbc6f94a4d11f390378470c1c3
AxelESSONO <naxelcdric@gmail.com>
2025-10-15 09:27:55 +0200

    Added New list and drawLine

--- app/src/main/java/fr/dawan/noteapp/navigation/BottomNavigationBar.kt
+++ app/src/main/java/fr/dawan/noteapp/navigation/BottomNavigationBar.kt
@@ -4,21 +4,42 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import fr.dawan.noteapp.ui.theme.ButtonBg
 import fr.dawan.noteapp.ui.theme.CardBg
+import fr.dawan.noteapp.ui.theme.SeparatorColor
 
+@Preview(showSystemUi = true, showBackground = true)
 @Composable
 fun BottomNavigationBar(
-    navController: NavHostController,
+    navController: NavHostController = rememberNavController(),
 ) {
     NavigationBar(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth()
+            .drawBehind {
+                val strokeWidth = 2.dp.toPx()
+                drawLine(
+                    color = SeparatorColor,
+                    start = Offset(0f, 0f),// Départ coin haut gauche
+                    end = Offset(size.width, 0f), // Fin coin haut droit
+                    strokeWidth = strokeWidth // Épaisseur du trait
+                )
+            },
         containerColor = CardBg
     ){
-        val backStackEntry = navController.currentBackStackEntry
+        val backStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = backStackEntry?.destination?.route
 
         NavBarItems.BarItems.forEach { navItem ->
@@ -35,7 +56,14 @@ fun BottomNavigationBar(
                 },
                 label = {
                     Text(text = navItem.title)
-                }
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = ButtonBg,
+                    selectedTextColor = ButtonBg,
+                    unselectedIconColor = SeparatorColor,
+                    unselectedTextColor = SeparatorColor,
+                    indicatorColor = CardBg
+                )
             )
         }
     }
