8050fc993b7b1c8f94caf4fb6eda1189ae297d51
AxelESSONO <naxelcdric@gmail.com>
2025-10-15 15:52:48 +0200

    Added Note ViewModel

---
+++ app/src/main/java/fr/dawan/noteapp/viewmodel/NoteViewModel.kt
@@ -0,0 +1,58 @@
+package fr.dawan.noteapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import fr.dawan.noteapp.model.Note
+import fr.dawan.noteapp.model.NoteRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+class NoteViewModel(
+    private val noteRepository: NoteRepository
+) : ViewModel(){
+
+    private var _notes : MutableStateFlow<List<Note>> = MutableStateFlow(
+        emptyList()
+    )
+    val notes : StateFlow<List<Note>> = _notes
+
+    fun insertNote(note: Note) = viewModelScope.launch {
+        noteRepository.insertNote(note)
+    }
+
+    fun deleteNote(note: Note) = viewModelScope.launch {
+        noteRepository.deleteNote(note)
+        getAllNotes()
+    }
+
+    fun updateNote(note: Note) = viewModelScope.launch {
+        if(note.title.isBlank() || note.content.isBlank()){
+            return@launch
+        }
+        noteRepository.updateNote(note)
+    }
+
+    fun getAllNotes() = viewModelScope.launch {
+        noteRepository.getNotes().collectLatest { notesList ->
+            _notes.value = notesList
+        }
+    }
+
+    fun getNoteById(id: Int) = viewModelScope.launch {
+        noteRepository.getNoteById(id)
+    }
+
+    fun deleteAllNotes() = viewModelScope.launch {
+        noteRepository.deleteAllNotes()
+        getAllNotes()
+    }
+
+
+    fun researchNotes(query: String) = viewModelScope.launch {
+        noteRepository.researchNotes(query).collectLatest { notesList ->
+            _notes.value = notesList
+        }
+    }
+}
