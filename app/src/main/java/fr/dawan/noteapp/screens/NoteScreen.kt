d7ba52f49f4ed16d88bd30aa99f6c0db7a8a7953
AxelESSONO <naxelcdric@gmail.com>
2025-10-15 10:47:12 +0200

    Added Contextual Menu

--- app/src/main/java/fr/dawan/noteapp/screens/NoteScreen.kt
+++ app/src/main/java/fr/dawan/noteapp/screens/NoteScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -23,6 +24,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
@@ -40,6 +43,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -70,13 +74,74 @@ fun NoteScreen(
 
     val navController = rememberNavController()
     var showPopup by remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
+    val options = listOf("Jaune", "Vert", "Bleu")
+    val colors = listOf(Color.Yellow, Color.Green, Color.Blue)
+    var colorSelected by remember { mutableStateOf(colors[0]) }
+
+
+    if (expanded){
+        Box(
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .wrapContentHeight()
+                .background(
+                    color = ButtonBg,
+                    shape = RoundedCornerShape(20.dp)
+                )
+                .padding(10.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(
+                onClick = { expanded = true },
+                modifier = Modifier.fillMaxWidth(0.8f)
+            ) {
+                Text(text = "Choisir une couleur")
+            }
+
+            // Menu Contextuel
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false },
+
+            ) {
+                options.forEach { option ->
+                   Row(
+                       modifier = Modifier.fillMaxWidth(),
+                       horizontalArrangement = Arrangement.SpaceBetween,
+                       verticalAlignment = Alignment.CenterVertically
+                   ) {
+                       DropdownMenuItem(
+                           modifier = Modifier.wrapContentSize(),
+                           text = { Text(text = option) },
+                           onClick = {
+                               colorSelected = colors[options.indexOf(option)]
+                               expanded = false
+                           },
+                           trailingIcon = {
+                               Box(
+                                   modifier = Modifier
+                                       .size(20.dp)
+                                       .background(
+                                           color = colors[options.indexOf(option)],
+                                           shape = RoundedCornerShape(5.dp)
+                                       )
+                               )
+                           }
+                       )
+                   }
+                }
+            }
+        }
+    }
 
     if (showPopup){
         Popup(
             alignment = Alignment.Center,
             onDismissRequest = { showPopup = false },
             properties = PopupProperties(focusable = false)
-        ) {
+        )
+        {
             Box(modifier = Modifier
                 .fillMaxWidth(0.8f)
                 .wrapContentHeight()
@@ -137,7 +202,7 @@ fun NoteScreen(
         floatingActionButton = {
             FloatingActionButton(
                 onClick = {
-                    showPopup = true
+                    expanded = true
                 },
                 containerColor = ButtonBg,
                 contentColor = TextColor
